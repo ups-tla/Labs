1995d82930e22b0c6baa78d7375531f2323a8dd9
Doug Hamilton <doug.hamil@gmail.com>
2011-10-17 20:30:55 -0700

    Lab 01 initial commit

---
+++ 01/src/org/tla/MobileLab01Activity.java
@@ -0,0 +1,13 @@
+package org.tla;
+
+import com.phonegap.*;
+import android.os.Bundle;
+
+public class MobileLab01Activity extends DroidGap {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.loadUrl("file:///android_asset/www/index.html");
+    }
+}
